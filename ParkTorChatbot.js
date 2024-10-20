957b9330f2d53f8ba2621378bf813b82fd1a27d9
Bradleysmu2024 <b.joandrey.2024@computing.smu.edu.sg>
2024-10-20 17:40:05 +0800

    Delete ParkTorChatbot.js

--- ParkTorChatbot.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('dfMessengerLoaded', function() {
-    document.getElementById('cta-chatbot').addEventListener('click', function(event) {
-        event.preventDefault();
-        openChatbot();
-    });
-});
